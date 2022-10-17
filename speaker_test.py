6d712ed4a1c29cd8fa570d4e65468138fca77b3e
Chris Fonseca <pl234142@ahschool.com>
2022-10-17 04:10:20 -0400

    first commit

---
+++ speaker_test.py
@@ -0,0 +1,5 @@
+# https://forums.raspberrypi.com/viewtopic.php?t=304566
+# thread above looks good, consult for more I2S stuff
+
+# https://bytesnbits.co.uk/raspberry-pi-i2s-sound-output/
+# scratch that, use this
