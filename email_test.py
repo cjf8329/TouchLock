bb5da0ceed4e1de85679014c92874d1ca39dfb97
Chris Fonseca <cjf8329@nyu.edu>
2022-11-01 09:36:48 -0400

    Email attachments

--- email_test.py
+++ email_test.py
@@ -1,16 +1,31 @@
 import smtplib
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
+
+mail_text = 'dfhfxchgxdggdx Balls'
 
 email_address = "touchlock.biolock@gmail.com"
 email_password = "igxvftioggpjopgg"
 
-# create email
-msg = EmailMessage()
+# create email header and text
+msg = MIMEMultipart()
 msg['Subject'] = "Email test"
 msg['From'] = email_address
-msg['To'] = "insert_email_here@gmail.com"
-msg.set_content("https://www.youtube.com/watch?v=bmtHdZlsiO8")
+msg['To'] = "karantsukheja@gmail.com"
+msg.attach(MIMEText(mail_text, 'plain'))
+
+#attach image
+file_name = 'ball.png'
+attached_file = open(file_name, 'rb')
+
+payload = MIMEBase("application", 'image')
+payload.set_payload((attached_file).read())
+encoders.encode_base64(payload)
+payload.add_header("Content-Disposition", "attachment", filename=file_name)
+msg.attach(payload)
 
 # send email
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
